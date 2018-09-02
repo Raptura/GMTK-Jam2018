07d491adc624ca302e4e21571f8c00f5285dafff
Armond Smith <smithark97@gmail.com>
2018-09-01 22:15:06 -0400

    Created Cardboard Box, added Score System

--- GMTK2018/Assets/Code/GameManager.cs
+++ GMTK2018/Assets/Code/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
         evidenceFound = new Dictionary<Evidence, int>();
 
-        if(gm == null)
+        if (gm == null)
             gm = this;
     }
 
@@ -31,11 +31,39 @@ public class GameManager : MonoBehaviour
             evidenceFound.Add(e, choice);
     }
 
-    public bool isCorrectEvidence(int s)
+    public int getScore()
+    {
+        int score = 0;
+        foreach (KeyValuePair<Evidence, int> entry in evidenceFound)
+        {
+            Evidence e = entry.Key;
+            int choice = entry.Value;
+            switch (choice)
+            {
+                case 1:
+                    score += e.score1;
+                    break;
+                case 2:
+                    score += e.score2;
+                    break;
+                case 3:
+                    score += e.score3;
+                    break;
+                default:
+                    score += 0;
+                    break;
+            }
+
+        }
+
+        return score;
+    }
+
+    public bool isCorrectEvidence()
     {
         //TODO: finish Evidence sequence
         //pick a random number, if it's less than or equal to the score, the case is plausible
-        if (Random.Range(1, 101) <= s)
+        if (Random.Range(1, 101) <= getScore())
         {
             return true;
         }
@@ -49,14 +77,17 @@ public class GameManager : MonoBehaviour
     /// Builds the evidence log to be used during the case
     /// </summary>
     /// <returns></returns>
-    public List<string> buildEvidenceLog() {
+    public List<string> buildEvidenceLog()
+    {
         List<string> log = new List<string>();
 
-        foreach (KeyValuePair<Evidence, int> entry in evidenceFound) {
+        foreach (KeyValuePair<Evidence, int> entry in evidenceFound)
+        {
             Evidence e = entry.Key;
             int choice = entry.Value;
 
-            switch (choice) {
+            switch (choice)
+            {
                 case 1:
                     log.Add(e.case_option_1);
                     break;
