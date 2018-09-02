4eb634ad1381923a2411d97cb83589444d332a1e
Ruby <fieryarbiter@gmail.com>
2018-09-02 15:44:14 -0400

    Added sprite animations

--- GMTK2018/Assets/Code/Player.cs
+++ GMTK2018/Assets/Code/Player.cs
@@ -5,33 +5,88 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     private float movSpeed = 2; // units per second
+    public float updateDelay = 0.5f;
+    public int Frame;
+    public float lastChanged;
+    public int lastDir;
+    
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (Time.time - lastChanged > updateDelay)
+        {
+            Frame += 1;
+            Frame = Frame % 2;
+            lastChanged = Time.time;
+            if (!Input.anyKey)
+            {
+                SpriteRenderer sr = GetComponent<SpriteRenderer>();
+                Sprite[] spritesb = Resources.LoadAll<Sprite>("player_back");
+                Sprite[] spritesf = Resources.LoadAll<Sprite>("player_front");
+                Sprite[] spritesr = Resources.LoadAll<Sprite>("player_right");
+                Sprite[] spritesl = Resources.LoadAll<Sprite>("player_left");
+                switch (lastDir)
+                {
+                    case 0:
+                        sr.sprite = spritesb[2];
+                        break;
+                    case 1:
+                        sr.sprite = spritesf[2];
+                        break;
+                    case 2:
+                        sr.sprite = spritesl[2];
+                        break;
+                    case 3:
+                        sr.sprite = spritesr[2];
+                        break;
+                }
+            }
+        }
+
         handleInput();	
 	}
 
 
     void handleInput()
     {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Sprite[] spritesb = Resources.LoadAll<Sprite>("player_back");
+        Sprite[] spritesf = Resources.LoadAll<Sprite>("player_front");
+        Sprite[] spritesr = Resources.LoadAll<Sprite>("player_right");
+        Sprite[] spritesl = Resources.LoadAll<Sprite>("player_left");
         if (GameManager.gm.paused)
             return;
 
         //ULDR Spacebar
         if (Input.GetKey(KeyCode.UpArrow))
+        {
             transform.Translate(Vector2.up * Time.deltaTime * movSpeed);
+            sr.sprite = spritesb[Frame];
+            lastDir = 0;
+        }
         if (Input.GetKey(KeyCode.DownArrow))
+        {
             transform.Translate(Vector2.down * Time.deltaTime * movSpeed);
+            sr.sprite = spritesf[Frame];
+            lastDir = 1;
+        }
         if (Input.GetKey(KeyCode.LeftArrow))
+        {
             transform.Translate(Vector2.left * Time.deltaTime * movSpeed);
+            sr.sprite = spritesl[Frame];
+            lastDir = 2;
+        }
         if (Input.GetKey(KeyCode.RightArrow))
+        {
             transform.Translate(Vector2.right * Time.deltaTime * movSpeed);
+            sr.sprite = spritesr[Frame];
+            lastDir = 3;
+        }
 
         //WASD E/F
 
